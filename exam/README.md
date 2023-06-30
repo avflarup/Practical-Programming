d0567904c3baba7d9230e32e807e4052da73576f
au681379 <au681379@uni.au.dk>
2023-06-30 12:31:54 +0200

    Final push. Pushing everything

--- exam/README.md
+++ exam/README.md
@@ -1,6 +1,8 @@
 I have made exam project 20.
 
-I would give myself 10 points as I have implemented a two-step stepper as described by the book.
+I would give myself 10 points as I have implemented a two-step stepper as described by the book. 
+It uses the embedded Runge-Kutta stepper of order 1, 2 as described in homework ODE for the first step. 
+
 It has been tested on 2 differential equations:
 u'' = u and u'' = -u
 and it seems to work.
--- homeworks/neural_network/ann.cs
+++ homeworks/neural_network/ann.cs
@@ -5,12 +5,34 @@ public class ann{
    Func<double,double> f = x => x*Exp(-x*x); /* activation function */
    vector p; /* network parameters */
    ann(int n){/* constructor */
-    = n;
+   this.n = n;
+   this.f = f;
+   this.p = p;
    }
-   double response(double x){
+   public double response(double x){
       /* return the response of the network to the input signal x */
+	   // Is the weighted sum
+	double Fp = 0;
+	for(int i=0; i<this.n; i++){
+		F+= this.f((x-p[3*i])/p[3*i+1])*p[3*i+2];
+	}
+	return F;
      }
-   void train(vector x,vector y){
+   public void train(vector x,vector y){
       /* train the network to interpolate the given table {x,y} */
+	   this.x = x;
+	   this.y = y;
+	   Func<vector, double> C = p => {
+		double cost = 0;
+		for(int i=0; i<x.size; i++){
+			cost += (response(x[i])-y[i])*(response(x[i])-y[i])
+		}
+	   }
+	   vector pp = p;
+	   var steps = qnewton.min(C, ref pp);
+
+	   
+
+
    }
 }
